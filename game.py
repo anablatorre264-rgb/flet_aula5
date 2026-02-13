8d89d45c882cd7f754484e2af8ee7423d7e251e3
analatorre <ana.b.silva264@aluno.senai.br>
2026-02-13 12:00:10 -0300

    raça do dog

--- game.py
+++ game.py
@@ -1,6 +1,17 @@
 import flet as ft
 
 def main(page: ft.Page):
+    mensagem = ft.Text("Escolha a raça correta do cachorro!!", color="#4d3018")
+    resposta_correta = "Pastor Alemão"
+
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "Parabéns, você acertou!!"
+        else:
+            mensagem.value = "Resposta errada"    
+        page.update()
+
+    page.bgcolor = "#dbc79d"
     page.title = "Game: Adivinhe a imagem"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -16,8 +27,34 @@ def main(page: ft.Page):
                 ft.Image(
                     src="images/zaza.jpg",
                     height=200
-                )
-            ]
+                ),
+                ft.Row(
+                    controls=[
+                        ft.Button(
+                            content="Spitz-Alemão",
+                            bgcolor="brown",
+                            color="white",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Pastor Alemão",
+                            bgcolor="brown",
+                            color="white",
+                            on_click=verificar_resposta
+                        ),
+                        ft.Button(
+                            content="Golden Retriever",
+                            bgcolor="brown",
+                            color="white",
+                            on_click=verificar_resposta
+                        )
+                    ],
+                    alignment=ft.CrossAxisAlignment.CENTER,
+                    wrap=True
+                ),
+                mensagem
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
     )
 
